54ef4aff4ce962b002feb09275a314a7ac0ec7c6
Huda Aldadah <hudaaldadah@gmail.com>
2025-11-14 13:57:36 -0600

    change project title

--- GeometryExhibition/HomeView.swift
+++ GeometryExhibition/HomeView.swift
@@ -37,7 +37,7 @@ struct HomeView: View {
                 }
             }
             .padding()
-            .navigationTitle("Geometry Exhibition Demo")
+            .navigationTitle("The geometry lab rats")
             .navigationBarTitleDisplayMode(.inline)
         }
     }
